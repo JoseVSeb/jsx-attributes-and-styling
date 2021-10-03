0070893a2c94f7d2741d0bab287e06e3b56cf268
Jose V Sebastian <josevseb99@gmail.com>
2021-10-04 01:18:44 +0530

    complete challenge

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200/300";
+
 ReactDOM.render(
   <div>
     <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <div>
+      <img src={img} />
+      <img className="food-img" src="https://picsum.photos/id/237/200/300" />
+      <img
+        className="food-img"
+        src="https://picsum.photos/seed/picsum/200/300"
+      />
+      <img className="food-img" src="https://picsum.photos/200/300?grayscale" />
+    </div>
   </div>,
   document.getElementById("root")
 );
